162ee6e037651141b1ddac980152f4e1eb7e7f0f
禹丹丹 <yudandanabc@gmail.com>
2021-03-11 23:30:33 +0800

    4

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,17 @@
+
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	"?extends": "插件 (根据项目实际情况安装自定义插件) ",
-	extends: ["eslint:recommended", "plugin:react/recommended"],
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 12,
-		sourceType: "module",
-	},
-	plugins: ["react"],
-	rules: {
-		indent: ["error", 4],
-		"linebreak-style": ["error", "windows"],
-		quotes: ["error", "single"],
-		semi: ["error", "always"],
-	},
-};
+    "extends": ["eslint-config-ali","prettier", "plugin:prettier/recommended"],
+    "parser": "babel-eslint",
+    "rules": {
+      "prettier/prettier": "error",
+      "strict": "off",
+      "no-console": "off",
+      "import/no-dynamic-require": "off",
+      "global-require": "off",
+      "require-yield": "off",
+    },
+    "plugins": ["prettier"],
+    "globals": {
+      "React": "readable"
+    }
+  }
