7770b1df7ac7e8ac8d9229c996be267fdb91cb78
yudandan <yudandanabc@gmail.com>
2021-03-17 17:40:18 +0800

    webpack

--- webpack-init/src/index.js
+++ webpack-init/src/index.js
@@ -1,8 +1,8 @@
 import _ from "lodash";
 // import "./style.css";
-// import Icon from "./icon.jpeg";
-// import Data from "./data.xml";
-// import Notes from "./data.csv";
+// // import Icon from "./icon.jpeg";
+// // import Data from "./data.xml";
+// // import Notes from "./data.csv";
 import printMe from "./print";
 
 function component() {
@@ -13,9 +13,9 @@ function component() {
   element.innerHTML = _.join(["Hello", "webpack"], " ");
 
   btn.innerHTML = "click me and check the console";
-  btn.onclick = printMe;
+  btn.onclick = printMe.bind(null, "nihao");
 
-  // element.classList.add("hello");
+  element.classList.add("hello");
 
   // // 将图片添加到div中
   // const myIcon = new Image();
@@ -30,3 +30,50 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+// async function getComponent() {
+//     const element = document.createElement("div");
+//     const { default: _ } = await import("lodash");
+//     // return import("lodash")
+//     //     .then(({ default: _ }) => {
+//     //         const element = document.createElement("div");
+//     //         element.innerHTML = _.join(["hello", "webpack"], " ");
+
+//     //         return element;
+//     //     })
+//     //     .catch((error) => {
+//     //         "an error occurred while loading the component";
+//     //     });
+
+//     element.innerHTML = _.join(["你好"], " ");
+//     return element;
+// }
+
+// getComponent().then((component) => {
+//     document.body.appendChild(component);
+// });
+
+// import _ from 'lodash';
+// import numRef from './ref.json';
+
+// export function numToWord(num) {
+//   return _.reduce(
+//     numRef,
+//     (accum, ref) => {
+//       return ref.num === num ? ref.word : accum;
+//     },
+//     ''
+//   );
+// }
+
+// export function wordToNum(word) {
+//   return _.reduce(
+//     numRef,
+//     (accum, ref) => {
+//       return ref.word === word && word.toLowerCase() ? ref.num : accum;
+//     },
+//     -1
+//   );
+// }
+console.log("index");
+// 这个 libra
--- webpack-init/webpack.config.js
+++ webpack-init/webpack.config.js
@@ -2,34 +2,58 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: {
-    index: "./src/index.js",
-    print: "./src/print.js"
+    // dependOn设置共享模块
+    index: {
+      import: "./src/index.js"
+      // dependOn: "shared",
+    }
+    // another: {
+    //     import: "./src/another-module.js",
+    //     dependOn: "shared",
+    // },
+    // shared: "lodash",
   },
   output: {
-    filename: "[name].bundle.js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
     clean: true, //  每次打包都清理dist文件
     publicPath: "/"
   },
+  // optimization: {
+  //     // 一个页面有多个入口时设置
+  //     runtimeChunk: "single",
+  //     // 将公共依赖提取到已有入口的chunk中
+  //     splitChunks: {
+  //         // chunks: "all",
+  //         cacheGroups: {
+  //             vendor: {
+  //                 test: /[\\/]node_modules[\\/]/,
+  //                 name: "vendors",
+  //                 chunks: "all",
+  //             },
+  //         },
+  //     },
+  // },
   devtool: "inline-source-map",
   devServer: {
-    contentBase: "./dist"
+    contentBase: "./dist",
+    hot: true
   },
   // plugins
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Development"
+      title: "Cacaing"
     })
   ],
   module: {
     rules: [
       // 样式
-      {
-        test: /\.(css|less|sass)$/i,
-        use: ["style-loader", "css-loader"]
-      },
+      // {
+      //     test: /\.(css|less|sass)$/i,
+      //     use: ["style-loader", "css-loader"],
+      // },
       // 图片
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -49,6 +73,25 @@ module.exports = {
         test: /\.(xml$)/i,
         use: ["xml-loader"]
       }
+      // loader
+      // {
+      //     test: /\.js$/,
+      //     // 将loader应用实际需要将其转换的模块
+      //     include: path.resolve(__dirname, "src"),
+      //     loader: "babel-loader",
+      // },
+      // // ts-loader
+      // {
+      //     test: /\.tsx$/,
+      //     use: [
+      //         {
+      //             loader: "ts-loader",
+      //             options: {
+      //                 transpileOnly: true,
+      //             },
+      //         },
+      //     ],
+      // },
     ]
   }
 };
