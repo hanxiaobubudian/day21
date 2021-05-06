71c7d5c9e4bc94767ff79464bdda78d8a38975d1
禹丹丹 <yudandanabc@gmail.com>
2021-05-06 14:02:03 +0800

    2

--- webpack-init/webpack.config.js
+++ webpack-init/webpack.config.js
@@ -1,97 +1,103 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "production",
-  entry: {
-    // dependOn设置共享模块
-    index: {
-      import: "./src/index.js"
-      // dependOn: "shared",
-    }
-    // another: {
-    //     import: "./src/another-module.js",
-    //     dependOn: "shared",
-    // },
-    // shared: "lodash",
-  },
-  output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true, //  每次打包都清理dist文件
-    publicPath: "/"
-  },
-  // optimization: {
-  //     // 一个页面有多个入口时设置
-  //     runtimeChunk: "single",
-  //     // 将公共依赖提取到已有入口的chunk中
-  //     splitChunks: {
-  //         // chunks: "all",
-  //         cacheGroups: {
-  //             vendor: {
-  //                 test: /[\\/]node_modules[\\/]/,
-  //                 name: "vendors",
-  //                 chunks: "all",
-  //             },
-  //         },
-  //     },
-  // },
-  devtool: "inline-source-map",
-  devServer: {
-    contentBase: "./dist",
-    hot: true
-  },
-  // plugins
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Cacaing"
-    })
-  ],
-  module: {
-    rules: [
-      // 样式
-      // {
-      //     test: /\.(css|less|sass)$/i,
-      //     use: ["style-loader", "css-loader"],
-      // },
-      // 图片
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource"
-      },
-      // 字体
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource"
-      },
-      // 加载数据
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ["csv-loader"]
-      },
-      {
-        test: /\.(xml$)/i,
-        use: ["xml-loader"]
-      }
-      // loader
-      // {
-      //     test: /\.js$/,
-      //     // 将loader应用实际需要将其转换的模块
-      //     include: path.resolve(__dirname, "src"),
-      //     loader: "babel-loader",
-      // },
-      // // ts-loader
-      // {
-      //     test: /\.tsx$/,
-      //     use: [
-      //         {
-      //             loader: "ts-loader",
-      //             options: {
-      //                 transpileOnly: true,
-      //             },
-      //         },
-      //     ],
-      // },
-    ]
-  }
+	// 通过配置mode=development 或者 mode=production 来制定是开发环境打包还是生产环境打包。
+	// 比如生产环境代码需要压缩，图片需要优化，webpack默认mode是生产环境，即mode=production。如果不设置会有警告
+	mode: 'production', // 环境 development production
+	entry: {
+		// dependOn设置共享模块
+		index: {
+			import: './src/index.js',
+			// dependOn: "shared",
+		},
+		// another: {
+		//     import: "./src/another-module.js",
+		//     dependOn: "shared",
+		// },
+		// shared: "lodash",
+	},
+	output: {
+		filename: '[name]-[hash:8].js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true, //  每次打包都清理dist文件
+		publicPath: '/',
+	},
+	// optimization: {
+	//     // 一个页面有多个入口时设置
+	//     runtimeChunk: "single",
+	//     // 将公共依赖提取到已有入口的chunk中
+	//     splitChunks: {
+	//         // chunks: "all",
+	//         cacheGroups: {
+	//             vendor: {
+	//                 test: /[\\/]node_modules[\\/]/,
+	//                 name: "vendors",
+	//                 chunks: "all",
+	//             },
+	//         },
+	//     },
+	// },
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist',
+		hot: true,
+	},
+	// plugins,webpack的支柱功能，作用于webpack整个构建周期，plugin插件的目的在于结局loader无法实现的任务
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Cacaing',
+		}),
+	],
+	// webpack是基于node.js的，项目中的任何文件都可以看成module.
+	module: {
+		// module.noParse    可以让webpack忽略对部分没采用模块化的文件的递归解析和处理，提高性能
+		// module.rules      在处理模块时，将符合规则条件的模块，提交给对应的处理器来处理，通常用来配置loader.其类型是一个数组，数组里每一项都描述了如何去处理部分的文件
+		rules: [
+			// 样式
+			{
+				test: /\.(css|less|sass)$/i,
+				use: ['style-loader', 'css-loader', 'less-loader'],
+			},
+			// 图片
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+			// 字体
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
+			},
+			// 加载数据
+			{
+				test: /\.(csv|tsv)$/i,
+				use: ['csv-loader'],
+			},
+			{
+				test: /\.(xml$)/i,
+				use: ['xml-loader'],
+			},
+			// 模块转化器，用于对模块源代码进行转换
+			// loader
+			// {
+			//     test: /\.js$/,
+			//     // 将loader应用实际需要将其转换的模块
+			//     include: path.resolve(__dirname, "src"),
+			//     loader: "babel-loader",
+			// },
+			// // ts-loader
+			// {
+			//     test: /\.tsx$/,
+			//     use: [
+			//         {
+			//             loader: "ts-loader",
+			//             options: {
+			//                 transpileOnly: true,
+			//             },
+			//         },
+			//     ],
+			// },
+		],
+	},
 };
--- 第2天/promise.js
+++ 第2天/promise.js
@@ -3,98 +3,105 @@
  * @Author: yudandan
  * @Date: 2021-03-18 15:31:11
  * @LastEditors: yudandan
- * @LastEditTime: 2021-03-19 11:42:06
+ * @LastEditTime: 2021-04-26 23:15:05
  */
 
-const { reject } = require('lodash');
-const { resourceLimits } = require('worker_threads');
+// const { reject } = require('lodash');
+// const { resourceLimits } = require('worker_threads');
 
 // const promise = new Promise((resolve, reject) => {
-//     // 成功时调用
-//     resolve('111');
-//     //   失败时调用
-//     reject('error');
+// 	// 成功时调用
+// 	resolve('111');
+// 	//   失败时调用
+// 	reject('error');
 // });
 
-function timeout(ms) {
-    return new Promise((resolve, reject) => {
-        setTimeout(resolve('成功1'), ms);
-    });
-}
+// function timeout(ms) {
+// 	return new Promise((resolve, reject) => {
+// 		setTimeout(resolve('成功1'), ms);
+// 	});
+// }
 
 // timeout(3000)
-//     .then((value) => {
-//         console.log('成功的值===', value);
-//     })
-//     .catch((err) => {
-//         console.log('error===', err);
-//     });
+// 	.then((value) => {
+// 		console.log('成功的值===', value);
+// 	})
+// 	.catch((err) => {
+// 		console.log('error===', err);
+// 	});
 // function timeout2(ms) {
-//     return new Promise((resolve, reject) => {
-//         // setTimeout(resolve(timeout(1000)), 1000);
-//         setTimeout(resolve(1), 1000);
-//     });
+// 	return new Promise((resolve, reject) => {
+// 		// setTimeout(resolve(timeout(1000)), 1000);
+// 		setTimeout(resolve(1), 1000);
+// 	});
 // }
 // timeout2()
-//     .then((result) => {
-//         console.log('result===', result);
-//         // return result * 2
-//         return new Promise((resolve, reject) => {
-//             resolve(8)
-//         })
-//     })
-//     .then(result2 => {
-//         console.log('result2===', result2)
-//         return result2 ** 2
-//     }, err => {
-//         console.log('哈哈哈err===', err)
-//     })
-//     .then(result => {
-//         console.log('result3===', result);
-//     })
-//     .catch((err) => {
-//         console.log('error===', err);
-//     });
+// 	.then((result) => {
+// 		console.log('result===', result);
+// 		// return result * 2
+// 		return new Promise((resolve, reject) => {
+// 			resolve(8);
+// 		});
+// 	})
+// 	.then(
+// 		(result2) => {
+// 			console.log('result2===', result2);
+// 			return result2 ** 2;
+// 		},
+// 		(err) => {
+// 			console.log('哈哈哈err===', err);
+// 		}
+// 	)
+// 	.then((result) => {
+// 		console.log('result3===', result);
+// 	})
+// 	.catch((err) => {
+// 		console.log('error===', err);
+// 	});
 
-// let promise = new Promise((resolve, reject) => {
-//     console.log('promise');
-//     resolve();
-// });
-// promise.then(() => {
-//     console.log('resolve');
-// });
-// console.log('hi');
+let promise = new Promise((resolve, reject) => {
+	console.log('promise');
+	resolve();
+});
+promise.then(() => {
+	console.log('resolve');
+});
+console.log('hi');
 
 // const getJSON = function(url) {
-//     const promise = new Promise((resolve, reject) => {
-//         const handler = function() {
-//             if(this.readyState !== 4) {
-//                 return;
-//             }
-//             if(this.status === 200) {
-//                 resolve(this.response)
-//             } else {
-//                 reject(new Error(this.statusText))
-//             }
-//         }
-//         const request = new XMLHttpRequest();
-//         request.open('GET', url);
-//         request.onreadystatechange = handler
-//         request.response = 'json'
-//         request.setRequestHeader('Accept', 'application/json')
-//         request.send()
-//     })
-//     return promise;
-// }
+// 	const promise = new Promise((resolve, reject) => {
+// 		const handler = function() {
+// 			if (this.readyState !== 4) {
+// 				return;
+// 			}
+// 			if (this.status === 200) {
+// 				resolve(this.response);
+// 			} else {
+// 				reject(new Error(this.statusText));
+// 			}
+// 		};
+// 		const request = new XMLHttpRequest();
+// 		request.open('GET', url);
+// 		request.onreadystatechange = handler;
+// 		request.response = 'json';
+// 		request.setRequestHeader('Accept', 'application/json');
+// 		request.send();
+// 	});
+// 	return promise;
+// };
 
-// getJSON('/posts.json').then((json) => {
-//     console.log('json===', json)
-// }, (error) => {
-//     console.log('error', error)
-// })
+// getJSON('/posts.json').then(
+// 	(json) => {
+// 		console.log('json===', json);
+// 	},
+// 	(error) => {
+// 		console.log('error', error);
+// 	}
+// );
 
 // finally -无论promise状态最终如何都会执行的操作
-//  格式
+// 格式;
+
 /**
  * promise
  * .then
@@ -103,14 +110,14 @@ function timeout(ms) {
  */
 // 手动实现promise.finally
 Promise.prototype.myFinally = function(callback) {
-    let P = this.constructor;
-    return this.then(
-        (value) => P.resolve(callback()).then(() => value),
-        (error) =>
-            P.resolve(callback()).then(() => {
-                throw error;
-            })
-    );
+	let P = this.constructor;
+	return this.then(
+		(value) => P.resolve(callback()).then(() => value),
+		(error) =>
+			P.resolve(callback()).then(() => {
+				throw error;
+			})
+	);
 };
 
 // Promise.all()-把多个Promise实例包装成新的Promise实例
@@ -154,33 +161,109 @@ Promise.prototype.myFinally = function(callback) {
 //     });
 // }
 
+// 第1题：
+// Promise.allSettled 返回执行promise的所有结果
+// 手动实现Promise.allSettle
+//  分析： 传入数组，返回 Promise， 执行每个promise  记录每种状态
+Promise.prototype.myAllSettled = function(promiseArr) {
+	const len = promiseArr.length;
+	if (len > 0) {
+		let returnArr = [];
+		return new Promise((resolve, reject) => {
+			promiseArr.forEach((item, index) => {
+				item.then((res) => {
+					returnArr.push({
+						status: 'fulfilled',
+						value: res,
+					});
+					if (index === len - 1) {
+						resolve(returnArr);
+					}
+				}).catch((error) => {
+					returnArr.push({
+						status: 'rejected',
+						error,
+					});
+					if (index === len - 1) {
+						reject(returnArr);
+					}
+				});
+			});
+		});
+	}
+};
+
+// 手动实现Promise.race
+//  分析：传入数组，返回 Promise， 返回最先执行的那个index
+Promise.prototype.myRace = function(promiseArr) {
+	const len = promiseArr.length;
+	if (len > 0) {
+		return new Promise((resolve, reject) => {
+			for (let i = 0; i < len; i++) {
+				let item = promiseArr[i];
+				item.then((res) => {
+					resolve(res);
+				}).catch((error) => {
+					reject(error);
+				});
+			}
+		});
+	}
+};
+
+// 手动实现Promise.all
+//  分析：传入数组，返回 Promise， 知道执行失败的结果
+Promise.prototype.myAll = function(promiseArr) {
+	const len = promiseArr.length;
+	if (len > 0) {
+		let returnArr = [];
+		return new Promise((resolve, reject) => {
+			for (let i = 0; i < len; i++) {
+				let item = promiseArr[i];
+				item.then((res) => {
+					returnArr.push({
+						status: 'fulfiled',
+						value: res,
+					});
+					if (index === len - 1) {
+						resolve(returnArr);
+					}
+				}).catch((error) => {
+					reject(error);
+				});
+			}
+		});
+	}
+};
+
 //  手动实现promise
 // 原理：使用回调函数，把回调函数封装在内部，使用上一直通过then方法链式调用。
 // 极简的实现
-// class MyPromise {
-//     callbacks = []; // 成功执行函数
-//     state = 'pending' // 状态
-//     value = null; // 保存结果
-//     constructor(fn) {
-//         fn(this._resolve.bind(this));
-//     }
-//     then(onFulfilled) {
-//         if(this.state === 'pending') {
-//             // 在resolve之前
-//             this.callbacks.push(onFulfilled);
-//         } else {
-//             // 在resolve之后，直接执行回调，返回结果
-//             onFulfilled(this.value)
-//         }
-//         this.callbacks.push(onFulfilled);
-//         return this;
-//     }
-//     _resolve(value) {
-//        this.state = 'fulfilled'
-//        this.value = value
-//        this.callbacks.forEach((fn) => fn(value));
-//     }
-// }
+class MyPromise {
+	callbacks = []; // 成功执行函数
+	state = 'pending'; // 状态
+	value = null; // 保存结果
+	constructor(fn) {
+		fn(this._resolve.bind(this));
+	}
+	then(onFulfilled) {
+		if (this.state === 'pending') {
+			// 在resolve之前
+			this.callbacks.push(onFulfilled);
+		} else {
+			// 在resolve之后，直接执行回调，返回结果
+			onFulfilled(this.value);
+		}
+		this.callbacks.push(onFulfilled);
+		return this;
+	}
+	_resolve(value) {
+		this.state = 'fulfilled';
+		this.value = value;
+		this.callbacks.forEach((fn) => fn(value));
+	}
+}
+
 // let p = new MyPromise((resolve) => {
 //     // setTimeout(() => {
 //     //     console.log('done');
@@ -196,15 +279,11 @@ Promise.prototype.myFinally = function(callback) {
 //     console.log('tip2', val)
 // });
 
-
 // setTimeout(() => {
 //     p.then(tip => {
 //         console.log('then3', tip)
 //     })
 // })
-
-
-
 
 // promise1 = new Promise((resolve, reject) => {
 //     setTimeout(()=>{
@@ -228,21 +307,30 @@ Promise.prototype.myFinally = function(callback) {
 //     console.log('--promise3--', promise3);
 // },3000)
 
+// function Foo() {
+// 	getName = function() {
+// 		console.log(1);
+// 	};
+// 	return this;
+// }
+// Foo.getName = function() {
+// 	console.log(2);
+// };
+// Foo.prototype.getName = function() {
+// 	console.log(3);
+// };
+// var getName = function() {
+// 	console.log(4);
+// };
+// function getName() {
+// 	console.log(5);
+// }
 
-function Foo() {
-    getName = function () { console.log (1); };
-    return this;
-}
-Foo.getName = function () { console.log (2);};
-Foo.prototype.getName = function () { console.log (3);};
-var getName = function () { console.log (4);};
-function getName() { console.log (5);}
-
-//请写出以下输出结果：
-Foo.getName(); // 2
-getName(); // 4
-Foo().getName(); // 报错
-getName(); // 4
-new Foo.getName(); // 2
-new Foo().getName(); // 3
-new new Foo().getName(); // 3
+// //请写出以下输出结果：
+// Foo.getName(); // 2
+// getName(); // 4
+// Foo().getName(); // 报错
+// getName(); // 4
+// new Foo.getName(); // 2
+// new Foo().getName(); // 3
+// new new Foo().getName(); // 3
